58e01b4079fdfcb56686fd21e8c6c5bbaf8fbe17
AzwadFawadHasan <106096161+AzwadFawadHasan@users.noreply.github.com>
2025-07-28 10:25:23 +0600

    arrays

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
-console.log("helo world");
-let age = 25;
-if (age < 50)
-    age += 5;
-console.log(`Age is now: ${age}`);
+let numbers = [1, 2, 3, 4, 5];
+let randomArray = [];
+randomArray[0] = 1;
+numbers.forEach((n) => {
+    console.log(n);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
-console.log("helo world");
+// console.log("helo world");
 
-let age: number = 25;
+// let age: number = 25;
 
-if (age < 50)
-    age += 5;
+// if (age < 50)
+//     age += 5;
 
-console.log(`Age is now: ${age}`);
+// console.log(`Age is now: ${age}`);
+
+
+// let sales = 123_456_789;
+// let course = "TypeScript";
+// let is_published = true;
+// let level;  // any type can be anything
+
+// function render(document: any) {
+//     console.log(document);
+// }   
+
+
+
+//declaring an arry
+let numbers: number[] = [1, 2, 3, 4, 5];
+let randomArray = [];
+randomArray[0] = 1;
+// number[0] = "hello";
+
+numbers.forEach((n) => {
+    console.log(n);
+});
