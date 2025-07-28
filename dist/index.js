3a59f682d6d86e042a2b044b8996e8e3a9abca17
AzwadFawadHasan <106096161+AzwadFawadHasan@users.noreply.github.com>
2025-07-28 10:05:57 +0600

    debugging

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,7 @@
 "use strict";
 console.log("helo world");
 let age = 25;
+if (age < 50)
+    age += 5;
+console.log(`Age is now: ${age}`);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 console.log("helo world");
 
 let age: number = 25;
+
+if (age < 50)
+    age += 5;
+
+console.log(`Age is now: ${age}`);
